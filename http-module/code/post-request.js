1aec43f2ca0bf8f353c1450ec4d9affd29bbccbb
Akshaya <chanchala9119@gmail.com>
2022-03-10 00:58:49 +0530

    Added HTTP Module

---
+++ http-module/code/post-request.js
@@ -0,0 +1,34 @@
+const http = require('http')
+let payload = JSON.stringify({
+    "name": "Harry",
+    "age": 28
+});
+let headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(payload, 'utf8')
+};
+
+let options = {
+    host: 'httpbin.org',
+    port: 443,
+    path: '/post',
+    method: 'POST',
+    headers: headers
+};
+
+let reqPost = http.request(options, (res) => {
+    console.log("status code: ", res.statusCode);
+
+    res.on('data', (chunks) => {
+
+        process.stdout.write(chunks);
+    });
+});
+
+reqPost.write(payload);
+reqPost.end();
+
+reqPost.on('error', (err) => {
+
+    console.error(err);
+});
