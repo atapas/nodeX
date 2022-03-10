98b9a0b0ff95db6498bbc2ea7213aafe6152a3cd
Akshaya <chanchala9119@gmail.com>
2022-03-10 21:24:16 +0530

    sensitive keys have been removed and HTTPS module is modified

--- https-module/code/create-server.js
+++ https-module/code/create-server.js
@@ -10,3 +10,14 @@ https.createServer(options, function (req, res) {
     })
     res.end('Hello World')
 }).listen(3000)
+
+//Run the following commands before running this file.
+//command = openssl req - nodes - new - x509 - keyout server.key - out server.cert
+// After running this command, we would get some options to fill.We can keep those options default or empty by entering ‘.‘ (dot).We would fill only two options for current as that would work fine for us.
+// Common Name(e.g.server FQDN or your name): localhost
+// Email Address: *************@****** (enter your email)
+// Other options such as Country Name, State or Province Name, Locality Name, Organization Name, and Organizational Unit Name are self - explanatory and also the system gives their example for help.
+
+//It is necessary to generate these files
+//server.cert = self-signed certificate file
+//server.key = Private key of the certificate
