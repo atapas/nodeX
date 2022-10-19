7d65f05ff59f2198a8d500625e61b6ec12ec9296
senali <senalidilumika@gmail.com>
2022-10-19 10:31:02 +0530

    feat: added errors module

---
+++ errors-module/code/reference-error.js
@@ -0,0 +1,14 @@
+/**
+ * Node.JS Errors Module
+ * 
+ * ReferenceError happens when a variable that doesn't exist in the current scope.
+ */
+
+// Variable that doesn't exist
+console.log(x);
+
+
+// Variable that hasn't yet been initialized
+console.log(y);
+const y = 10;
+ 
